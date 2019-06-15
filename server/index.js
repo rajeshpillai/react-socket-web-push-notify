77efe8035ea6ecb9040c441cee1b97d564c73a8a
Rajesh Pillai <pillai.rajsh@gmail.com>
2019-06-15 07:22:02 +0530

    Init repo with React and socket.io for web push notification example

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app  = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const port = 4000;
+
+http.listen(port, () => {
+  console.log(`Listening on ${port}`);
+  
+});
+    
+io.on('connection',function(socket){
+  // This event will trigger when any user is connected.
+  // You can use 'socket' to emit and receive events.
+  console.log('a user connected.');
+});
+
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+const socket = require('socket.io-client')('http://localhost:4000');
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React:Socket:Push Notification
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
