18af9b4f1708687776bafa9e2235761ccfea5f30
Rajesh Pillai <pillai.rajesh@gmail.com>
2019-06-16 15:04:25 +0530

    Added push support from web

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const webpush = require("web-push"); //requiring the web-push module
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,6 +13,25 @@ app.use(bodyParser.json());
 const port = 4000;
 
 const dummyDb = { subscription: null }; //dummy in memory store
+
+/* web push */
+const vapidKeys = {
+  publicKey:
+    "BLHxWiNVmr7ROB8O3KpPRJFAMhMypwe4X9TdWMmhsPSzHszo32PDkndpvWx3H0OY2HwFCQRU98JBpZ_AEsVxWG4",
+  privateKey: "VXcSZD3mdKyXEmDZNrB02WTxgUZmdIpAEo5tnXR4OH4"
+};
+//setting our previously generated VAPID keys
+webpush.setVapidDetails(
+  "mailto:coachingmonk@email.com",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend = "") => {
+  webpush.sendNotification(subscription, dataToSend);
+};
+
+/* <- web push */
 
 const saveToDatabase = async subscription => {
   // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
@@ -23,6 +43,14 @@ app.post("/save-subscription", async (req, res) => {
   const subscription = req.body;
   await saveToDatabase(subscription); //Method to save the subscription to Database
   res.json({ message: "success" });
+});
+
+//route to test send notification
+app.get("/send-notification", (req, res) => {
+  const subscription = dummyDb.subscription; //get subscription from your databse here.
+  const message = "Hello World from server";
+  sendNotification(subscription, message);
+  res.json({ message: "message sent" });
 });
 
 http.listen(port, () => {
